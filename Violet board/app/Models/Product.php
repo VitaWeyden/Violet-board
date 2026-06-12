83c88609f42ee653e6b74f85758449b282ade78b
Zsófia Gergely <145346080+SophieGergely@users.noreply.github.com>
2026-06-12 17:49:52 +0200

    starting portfolio version

---
+++ Violet board/app/Models/Product.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'discounted_price',
+        'is_discounted',
+        'is_best_seller',
+        'is_new',
+        'min_age',
+        'min_players',
+        'max_players',
+    ];
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+
+    public function getMainImagePath()
+    {
+        return asset('Pictures/' . Str::slug($this->name) . '1.jpg');
+    }
+
+    
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'category_product');
+    }
+
+    public function scopeDiscounted($query)
+    {
+        return $query->whereNotNull('discounted_price');
+    }
+   
+}
